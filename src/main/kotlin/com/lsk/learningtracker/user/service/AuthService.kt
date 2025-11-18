b1cbe104f411f6db6a3da0b34b98e5a2f12c37a7
cheers8oys <cheers8oys@gmail.com>
2025-11-18 18:22:41 +0900

    feat(AuthService): AuthService 구현 및 테스트

---
+++ src/main/kotlin/com/lsk/learningtracker/user/service/AuthService.kt
@@ -0,0 +1,25 @@
+package com.lsk.learningtracker.user.service
+
+import com.lsk.learningtracker.user.model.User
+import com.lsk.learningtracker.user.repository.UserRepository
+
+class AuthService(
+    private val userRepository: UserRepository
+) {
+
+    fun register(username: String, password: String): User {
+        val user = User(username, password)
+        return userRepository.save(user)
+    }
+
+    fun login(username: String, password: String): User {
+        val user = userRepository.findByUsername(username)
+            ?: throw IllegalArgumentException("존재하지 않는 사용자입니다.")
+
+        require(user.password == password) {
+            "비밀번호가 일치하지 않습니다."
+        }
+
+        return user
+    }
+}
